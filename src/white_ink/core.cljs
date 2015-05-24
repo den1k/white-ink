948eef14c3866bed42e4ecde5e8fa67d4ea26c94
den1k <kalavox@gmail.com>
2015-05-24 09:00:06 -0400

    wip: don't rerender when you caused the change. constantly toggling I should update works somehow.

--- src/white_ink/core.cljs
+++ src/white_ink/core.cljs
@@ -3,7 +3,8 @@
             [om.dom :as dom :include-macros true]
             [sablono.core :as html :refer-macros [html]]
             [white-ink.state :refer [app-state]]
-            [white-ink.utils.text :as text]))
+            [white-ink.utils.text :as text]
+            [cljs.core.async :as async]))
 
 (enable-console-print!)
 
@@ -54,6 +55,28 @@
       (om/build search-result-view search-res)
       (:text res))))
 
+;var setSelection = function(elem, start, end) {
+;                                               var range = document.createRange();
+;                                                   range.setStart(elem.firstChild, start);
+;                                                   range.setEnd(elem.firstChild, end);
+;                                                   var sel = window.getSelection();
+;                                               sel.removeAllRanges();
+;                                               sel.addRange(range);
+;                                               };
+
+(defn set-selection [node start end]
+  (let [range (doto
+                (.createRange js/document)
+                (.setStart (.-firstChild node) start)
+                (.setEnd (.-firstChild node) end))
+        sel (doto
+              (.getSelection js/window)
+              .removeAllRanges
+              (.addRange range))]))
+
+(defn set-cursor [node idx]
+  (set-selection node idx idx))
+
 (defn editor [{:keys [text] :as current-draft} owner]
   (reify
     om/IDisplayName
@@ -61,13 +84,23 @@
     om/IInitState
     (init-state [_]
       {:search-text nil
-       :cursor-pos nil})
+       :cursor-pos  nil
+       :should-update true})
+    om/IShouldUpdate
+    (should-update [_ np _]
+      (let [should-update (om/get-state owner :should-update)]
+        (om/set-state! owner :should-update true)
+        (.log js/console "np" (clj->js np))
+        should-update))
+    ;om/IDidUpdate
+    ;(did-update [_ _ _]
+    ;  (set-cursor (om/get-node owner "text") (om/get-state owner :cursor-pos)))
     om/IRenderState
     (render-state [_ {:keys [search-text]}]
       ;; don;t search on every render
       (let [text (if search-text
-                             (search->html (text/search text search-text))
-                             text)]
+                   (search->html (text/search text search-text))
+                   text)]
         #_(.log js/console "search" (clj->js text))
         (html [:div {:style styles-texts}
                [:input {:type        "text"
@@ -76,12 +109,13 @@
                         :on-change   #(om/set-state! owner :search-text (.. % -target -value))}]
                [:div {:style            (assoc styles-texts :height 200
                                                             :overflow "auto")
+                      :ref              "text"
                       :content-editable true
-
-                      :on-key-down      (fn [e]
+                      :on-key-up        (fn [e]
                                           (let [cursor-pos (.. js/window getSelection -anchorOffset)]
                                             (.log js/console "T" cursor-pos)
                                             (om/set-state! owner :cursor-pos cursor-pos)
+                                            (om/set-state! owner :should-update false)
                                             ; persist entire text in memory and send diff of change to backend
                                             (om/update! current-draft :text (.. e -target -textContent))))
                       }
@@ -117,13 +151,17 @@
              (om/build editor-view data)
              (om/build reviewer-view data))))
 
-(om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (om/build app data))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+(let [transactions (async/chan)
+      transactions-pub (async/pub transactions :tag)]
+  (om/root
+    (fn [data owner]
+      (reify om/IRender
+        (render [_]
+          (om/build app data))))
+    app-state
+    {:target    (. js/document (getElementById "app"))
+     :tx-listen (fn [tx] (async/put! transactions tx))
+     :shared    {:tx-chan transactions-pub}}))
 
 
 (defn on-js-reload []
