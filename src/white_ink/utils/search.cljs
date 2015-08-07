8b6fa4100783be05fdd23d2cdf983770afd3f538
den1k <kalavox@gmail.com>
2015-08-07 09:10:11 -0400

    make moving between search results work

--- src/white_ink/utils/search.cljs
+++ src/white_ink/utils/search.cljs
@@ -28,6 +28,34 @@
                 out
                 nil))))))))
 
+;; todo maybe delete this
+(defn find-idxs [text query]
+  (if (utils.text/empty-or-whitespace? query)
+    [[:text text]]
+    (let [q (.toLowerCase query)                            ; case insensitive
+          q-len (count q)
+          t-len (count text)
+          search-text (.toLowerCase text)]
+      (loop [prev-idx 0 out []]
+        (let [i (.indexOf search-text q prev-idx)]
+          (if-not (= -1 i)
+            (let [end-idx (+ i q-len)]
+              (recur end-idx
+                     (if-not (= prev-idx i)
+                       (conj out
+                             [:text prev-idx]
+                             [:res i])
+                       (conj out [:res i]))))
+            ;; last text segment
+            (if-not (= prev-idx t-len)
+              (conj out [:text prev-idx])
+              ;; return nil if no results
+              (if (< 1 (count out))
+                out
+                nil))))))))
+
+;(find-idxs "hello buddy" "bud")
+
 (defn constrain-query [q]
   (if (> 2 (count q))
     ""
@@ -42,17 +70,21 @@
 (defn next-res-idx [dir cur-idx search-results]
   {:pre [(number? cur-idx)]}
   (let [max-idx (dec (count search-results))
-        steps (case dir
-                :forward 2
-                :backward -2)
-        next-idx (+ cur-idx steps)]
-    (cond
-      ; going backwards and no more results
-      (neg? next-idx) (->> search-results
-                           reverse
-                           count-until-search-res
-                           (- max-idx)) #_(next-res-idx dir (count search-results) search-results)
-      ; passed in cur idx lies outside of search-results
-      (> next-idx max-idx) (-> search-results
-                               count-until-search-res)
-      :else next-idx)))
+        res (case dir
+              ; going backwards and no more results
+              :backward (->> (subvec search-results 0 cur-idx)
+                             reverse
+                             count-until-search-res)
+              ; passed in cur idx lies outside of search-results
+              :forward (-> (subvec search-results (inc cur-idx))
+                           count-until-search-res))
+        next-idx (do (prn "res1" res)
+                     (if res
+                       ((case dir
+                          :forward +
+                          :backward -) cur-idx res 1)
+                         (case dir
+                           :forward (next-res-idx :forward 0 search-results)
+                           :backward (next-res-idx :backward max-idx search-results))))]
+    (prn "nextidx" next-idx)
+    next-idx))
